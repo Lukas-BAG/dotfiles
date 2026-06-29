e18a791a18886aa02c43e8c96b5fee8678647765
Lukas-BAG <lukas.gruender@brockhaus-ag.de>
2026-06-29 19:24:37 +0200

    feat(alias): add the alias lsg for ls followed by a grep which I use often

--- modules/base/.config/dotfiles/aliases.d/base.sh
+++ modules/base/.config/dotfiles/aliases.d/base.sh
@@ -27,5 +27,6 @@ alias lsb="lsblk -f"
 alias show_file_sizes="du -h --max-depth=1 | sort -h -r"
 alias myrsync="rsync -av --info=progress2"
 alias pane-recolor="sh ~/Main/Scripts/Tmux/tmux-pane-color.sh"
+alias lsg="ls | grep -i $1"
 
 
